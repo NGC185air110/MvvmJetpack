92ea7fb4307e06c1757af74d311ffbeb4f0fbe60
Dinglicheng <1297740798@qq.com>
2021-06-15 19:32:15 +0800

    arotuer

--- app/src/main/java/com/example/mvvmjetpack/MainActivity.kt
+++ app/src/main/java/com/example/mvvmjetpack/MainActivity.kt
@@ -3,17 +3,21 @@ package com.example.mvvmjetpack
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.alibaba.android.arouter.launcher.ARouter
 import com.example.mvvmjetpack.databinding.ActivityMainBinding
 import com.example.mvvmjetpack.model.User
 import com.example.mvvmjetpack.view.FragmentDilog
 
-
+@Route(path = "/test/aaa")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val activityMainBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         var user: User = User("dinglicheng", "123456")
         activityMainBinding.userInfo = user
-        FragmentDilog().show(this.supportFragmentManager,"aaa")
+        //FragmentDilog().show(this.supportFragmentManager,"aaa")
+        ARouter.getInstance().build("/test/bbb").navigation()
+        activityMainBinding.btnGo.setOnClickListener { ARouter.getInstance().build("/test/bbb").navigation() }
     }
 }
--- app/src/main/java/com/example/mvvmjetpack/MainActivity2.kt
+++ app/src/main/java/com/example/mvvmjetpack/MainActivity2.kt
@@ -3,10 +3,12 @@ package com.example.mvvmjetpack
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.alibaba.android.arouter.facade.annotation.Route
 import com.example.mvvmjetpack.databinding.ActivityMain2Binding
 import com.example.mvvmjetpack.model.Goods
 import kotlin.random.Random
 
+@Route(path = "/test/bbb")
 class MainActivity2 : AppCompatActivity() {
     var goods: Goods? = null
     var activityMain2Binding : ActivityMain2Binding? = null
